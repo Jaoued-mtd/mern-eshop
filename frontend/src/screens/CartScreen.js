7daa2eed6feb731dc10199e96b68a570c466b1cf
Jaoued <68811444+Jaoued-mtd@users.noreply.github.com>
2020-11-24 13:07:06 +0100

    remove from cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -12,7 +12,7 @@ import {
   Card,
 } from "react-bootstrap";
 
-import { addToCart } from "../actions/cartActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -29,7 +29,7 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    console.log("remove");
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
