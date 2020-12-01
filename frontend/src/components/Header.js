c581bd767eb221445b2499609a385149a4526fa4
Jaoued <68811444+Jaoued-mtd@users.noreply.github.com>
2020-12-01 14:23:18 +0100

    navbar changed in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -32,6 +32,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
+            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
@@ -71,7 +72,6 @@ const Header = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Route render={({ history }) => <SearchBox history={history} />} />
     </header>
   );
 };
